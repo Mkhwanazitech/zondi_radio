00ecf970c7c409f61b85947246a142ca95b0fd95
mkhwanazig31-sudo <mkhwanazig31@gmail.com>
2026-09-15 23:53:36 +0200

    Update zondi.py

--- zondi.py
+++ zondi.py
@@ -125,7 +125,8 @@ def panic():
     conn.execute("INSERT INTO alerts (username,phone,address) VALUES (?,?,?)", (session['user'], u['phone'] if u else '', u['address'] if u else ''))
     conn.commit()
     conn.close()
-    socketio.emit('panic_alert', {'username': session['user']})
+        socketio.emit('panic_alert', {'username': session['user']})
+    return jsonify(ok=True)
     return jsonify(ok=True)
 
 @app.route('/patrol')
